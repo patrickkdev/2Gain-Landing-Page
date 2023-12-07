1e577a1a1e7e128c643174c793bc034ab58fc2c7
patrickkdev <patrick8q@gmail.com>
2023-12-07 17:37:51 -0300

    Add call to action 'peça um teste grátis'

--- components/Home/FloatingWhatsAppButton.tsx
+++ components/Home/FloatingWhatsAppButton.tsx
@@ -1,11 +1,15 @@
 import { gtag_report_conversion } from "@/lib/gtag";
-import { WhatsApp } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
+import { ArrowForward, ChevronRight, WhatsApp } from "@mui/icons-material";
+import { Card, IconButton, Typography } from "@mui/material";
 import React from "react";
 
 const FloatingWhatsAppButton = () => {
 	return (
-		<div style={{ pointerEvents: "none", position: "fixed", bottom: "0px", right: "0px", padding: "24px" }}>
+		<div style={{ pointerEvents: "none", position: "fixed", bottom: "0px", right: "0px", padding: "24px", display: "flex", gap: "12px", justifyContent: "center", alignItems: "center" }}>
+			<Card elevation={5} sx={{padding: "2px 8px", opacity: 0.8, backdropFilter: "blur(8px)"}}>
+				<Typography variant="h6">Peça um teste grátis </Typography>
+			</Card>
+			<ArrowForward />
 			<IconButton
 				onClick={() => {
 					gtag_report_conversion(window.location.href, "7wR9CKGn4foYEJDvlcgq");
